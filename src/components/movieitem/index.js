2db70301f3a74f5b46c3ac60affc5097459ca71a
Siavoush Redhai <SiavoushRedhai@siavoushs-mbp.mynet>
2021-01-13 19:58:16 +0000

    create movie item with resposive css

--- src/components/movieitem/index.js
+++ src/components/movieitem/index.js
@@ -1,31 +1,102 @@
 import React from "react";
 import styled from 'styled-components';
 
-export default class MovieItem extends React.Component {
+import * as colors from '../../colors';
 
-  render () {
-    return (
-      // The MovieItemWrapper must be linked to the movie details popup
-      <MovieItemWrapper>
-        <LeftCont>
-        </LeftCont>
-        <RightCont>
-        </RightCont>
-      </MovieItemWrapper>
-    )
-  }
+export default function MovieItem(props) {
+	return (
+		// The MovieItemWrapper must be linked to the movie details popup
+		<MovieItemWrapper>
+			<LeftCont>
+				<Poster 
+					src={`https://image.tmdb.org/t/p/w185/${ props.poster }`} 
+					alt={ props.title } 
+				/>
+			</LeftCont>
+
+			<RightCont>
+				<HeadingCont>
+					<Heading>{ props.title }</Heading>
+
+					<RatingCont>
+						<Rating>{ props.rating }</Rating>
+					</RatingCont>
+				</HeadingCont>
+
+				<Genres>genres</Genres>
+
+				<Overview>{ props.overview }</Overview>
+
+				<Release>{ props.release }</Release>
+			</RightCont>
+		</MovieItemWrapper>
+	)
 }
 
 const MovieItemWrapper = styled.div`
-  position: relative;
-  background-color: white;
-  border-radius: 3px;
+	display: flex;
+	position: relative;
+	background-color: white;
+	border-radius: 3px;
+	padding: 10px;
+	margin-top: 15px;
 `
-
 const LeftCont = styled.div`
-  display: inline-block;
+	margin: 10px;
 `
-
 const RightCont = styled.div`
-  display: inline-block;
+	display: flex;
+	flex-direction: column;
+	margin: 10px;
+`
+const Poster = styled.img`
+	width: 100px;
+
+	@media (min-width: 600px) {
+		width: auto;
+	}
+`
+const HeadingCont = styled.div`
+	display: flex;
+	justify-content: space-between;
+	align-items: flex-start;
+	flex-direction: column;
+	
+	@media (min-width: 600px) {
+		flex-direction: row;
+		align-items: center;
+	}
+`
+const Heading = styled.h2`
+	margin: 0;
+	font-size: 1.2rem;
+	
+	@media (min-width: 600px) {
+		font-size: 1.5rem;
+	}
+`
+const RatingCont = styled.div`
+	padding: 5px;
+	border-radius: 3px;
+	background-color: #d9e021;
+	margin-top: 10px;
+	
+	@media (min-width: 600px) {
+		margin-top: 0px;
+	}
+`
+const Rating = styled.p`
+	color: #ffffff;
+`
+const Genres = styled.p`
+	margin-top: 10px;
+	color: ${ colors.secondaryColor };
+	font-weight: bold;
+`
+const Overview = styled.p`
+	margin-top: 10px;
+	flex-grow: 1;
+`
+const Release = styled.p`
+	margin-top: 20px;
 `
