04f9202995aeb83f965c606adad7947da2892890
Siavoush Redhai <SiavoushRedhai@siavoushs-mbp.mynet>
2021-01-13 18:01:37 +0000

    map over movies and pass info as props

--- src/components/movielist/index.js
+++ src/components/movielist/index.js
@@ -4,18 +4,30 @@ import styled from 'styled-components';
 import MovieItem from '../movieitem';
 
 export default class MovieList extends React.Component {
+	render () {
+		const { movies, genres } = this.props;
 
-  render () {
-    const { movies, genres } = this.props;
-
-    return (
-      <MoviesWrapper>
-        {/* Finish the MovieItem component and use it here to display the movie results */}
-      </MoviesWrapper>
-    )
-  }
+		return (
+		<MoviesWrapper>
+			{/* Finish the MovieItem component and use it here to display the movie results */}
+			{movies.map(movie => (
+				<MovieItem 
+					key={ movie.id }
+					genres={ genres }
+					id={ movie.id }
+					poster={ movie.poster_path }
+					title={ movie.title }
+					overview={ movie.overview }
+					release={ movie.release_date }
+					rating={ movie.vote_average }
+					genre_ids={ movie.genre_ids }
+				/> )
+			)}
+		</MoviesWrapper>
+		)
+	}
 }
 
 const MoviesWrapper = styled.div`
-  position: relative;
+	position: relative;
 `
