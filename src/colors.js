0854ccac8786013357c36d7116ebc8771c50cb26
Siavoush Redhai <SiavoushRedhai@siavoushs-mbp.mynet>
2021-01-13 18:03:13 +0000

    change color name

--- src/colors.js
+++ src/colors.js
@@ -1,5 +1,5 @@
 export const sideNavBar = '#001e2d';
-export const sideNavBarHover = '#d9e021';
+export const secondaryColor = '#d9e021';
 export const fontColor = 'rgb(64, 78, 95)';
 export const primaryColor = '#001b29';
 export const lightBackground = '#f6f7f9';
