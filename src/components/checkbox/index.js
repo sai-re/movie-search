b71128f42168b51fd0f45fc4181450c50c0295aa
Siavoush Redhai <SiavoushRedhai@siavoushs-mbp.mynet>
2021-01-14 14:09:46 +0000

    create checkbox components and expandable filters

--- src/components/checkbox/index.js
+++ src/components/checkbox/index.js
@@ -1,17 +1,23 @@
 import React from "react";
 import styled from 'styled-components';
 
-export default class CheckBox extends React.Component {
-  // Create a custom checkbox component
-  
-  render () {
-    return (
-      <CheckboxCont>
-      </CheckboxCont>
-    )
-  }
+export default function CheckBox(props) {
+	// Create a custom checkbox component
+	return (
+		<CheckboxCont>
+			<CheckboxInput name={ props.name } type="checkbox"/>
+			<CheckboxLabel>{ props.name }</CheckboxLabel>
+		</CheckboxCont>
+	)
 }
 
 const CheckboxCont = styled.div`
-  position: relative;
+	margin-top: 10px;
+`
+
+const CheckboxInput = styled.input`
+	margin: 0 10px 0 0;
+`
+
+const CheckboxLabel = styled.label`
 `
--- src/components/expandablefilters/index.js
+++ src/components/expandablefilters/index.js
@@ -1,16 +1,106 @@
 import React from "react";
 import styled from 'styled-components';
 
+import * as colors from '../../colors';
+
 import Checkbox from "../../components/checkbox";
 
 export default class ExpandableFilters extends React.Component {
-  constructor (props) {
-    super(props);
+	constructor (props) {
+		super(props);
 
-    this.state = {
-      filtersShown: false
-    };
-  }
+		this.state = {
+			filtersShown: [false, false, false]
+		};
 
-  // You need to create your own checkbox component with a custom checkmark
+		this.handleExpand = this.handleExpand.bind(this);
+	}
+
+	/**
+	* @function listCheckBoxes maps over props
+	* @returns {JSX.Element} list of checkboxes
+	*/
+	listCheckBoxes = (filters) => filters.map(filter => <Checkbox key={ filter.id } name={ filter.name } />);
+
+	/**
+	* @function handleExpand copy state and togge state bool with index
+	*/
+	handleExpand = (index) => {
+		const filtersShown = [...this.state.filtersShown];
+		filtersShown[index] = !filtersShown[index];
+
+		this.setState({ filtersShown: filtersShown });
+	};
+
+	render() {
+		return (
+			<ExpandableFiltersCont>
+				<ExpandableFilter>
+					<Expand 
+						onClick={ () => this.handleExpand(0) }
+						visible={ this.state.filtersShown[0] } 
+					>
+						Select genre(s)
+					</Expand>
+
+					<List visible={ this.state.filtersShown[0] } >
+						{ this.listCheckBoxes(this.props.genres) }
+					</List>
+				</ExpandableFilter>
+
+				<ExpandableFilter>
+					<Expand 
+						onClick={ () => this.handleExpand(1) }
+						visible={ this.state.filtersShown[1] } 
+					>
+						Select min. vote
+					</Expand>
+
+					<List visible={ this.state.filtersShown[1] } >
+						{ this.listCheckBoxes(this.props.ratings) }
+					</List>
+				</ExpandableFilter>
+
+				<ExpandableFilter>
+					<Expand 
+						onClick={ () => this.handleExpand(2) }
+						visible={ this.state.filtersShown[2] } 
+					>
+						Select language
+					</Expand>
+
+					<List visible={ this.state.filtersShown[2] } >
+						{ this.listCheckBoxes(this.props.languages) }
+					</List>
+				</ExpandableFilter>
+			</ExpandableFiltersCont>
+		)
+	}
 }
+
+const ExpandableFiltersCont = styled.div`
+	margin-top: 10px;
+`
+
+const ExpandableFilter = styled.div`
+	&:not(:first-child) {
+		margin-top: 15px;
+	}
+`
+
+const Expand = styled.button`
+    background-color: #ffffff;
+    border: none;
+	font-size: 1rem;
+	color: ${ colors.primaryColor };
+	padding: 0;
+	
+	&:before {
+		content: '${ props => props.visible ? '-' : '+'}';
+		margin-right: 10px;
+	}
+`
+
+const List = styled.div`
+	display: ${ props => props.visible ? 'block' : 'none'}
+`
